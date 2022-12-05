c6e5bf89d92c4a9982710e83d36b5ade18797c0f
Guilherme Lopes <111246423+guilopesrbc@users.noreply.github.com>
2022-12-04 22:17:23 -0300

    Update script.js

--- exerc02/script.js
+++ exerc02/script.js
@@ -5,7 +5,7 @@ var final = document.getElementById("txtfim")
 var passo = document.getElementById("txtpasso")
 var resultado = document.getElementById("txtres")
 
-if (inicio.value.lenght == 0 || final.value.lenght == 0 || passo.value.lenght == 0){
+if (inicio.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
     window.alert("ERRO, DADOS NÃO PREENCHIDOS!")
 } else{
     resultado.innerHTML = "Sequência gerada: "
